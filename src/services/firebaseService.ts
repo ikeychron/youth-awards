9ce44666d71fe48c33221f20f2e8267db5b1d2d4
Carlos Barrios <juanbarrios045@gmail.com>
2024-02-05 04:20:27 -0400

    Main | Fix page

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,7 +1,8 @@
 import { database } from '@/firebase/firebase';
+import { TOldWinners } from '@/interfaces';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 
-export const getOldWinners = async () => {
+export const getOldWinners = async (): Promise<TOldWinners> => {
   try {
     const q = query(
       collection(database, 'old-winners'),
@@ -12,7 +13,7 @@ export const getOldWinners = async () => {
     return querySnapshot.docs.map((item) => ({
       id: item.id,
       ...item.data(),
-    }));
+    })) as TOldWinners;
   } catch (error) {
     console.error('Error getting data:', error);
     throw error;
